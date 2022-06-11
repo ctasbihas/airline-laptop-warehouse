925173fb4048f0de2fb281594754672b377d3790
ctasbihas <tasbihhmd@gmail.com>
2022-06-11 20:11:32 +0600

    hmm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const port = Process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
